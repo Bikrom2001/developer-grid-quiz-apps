d96b380d47cc17cb453f295846da6e8df7d321a9
Bikrom-Roy <bikromroy2001@gmail.com>
2022-10-12 01:51:37 +0600

    hero secation update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { Link, useLoaderData } from 'react-router-dom';
 import Category from '../Category/Category';
 
 const Home = () => {
@@ -8,12 +8,12 @@ const Home = () => {
         <div>
             <section className="dark:bg-gray-800 dark:text-gray-100">
                 <div className="container mx-auto flex flex-col items-center px-4 py-12 text-center md:py-16 md:px-10 lg:px-16 xl:max-w-3xl">
-                    <h1 className="text-4xl font-bold leading-none sm:text-5xl">Quisquam necessita vel
-                        <span className="dark:text-violet-400">laborum doloribus</span>delectus
+                    <h1 className="text-4xl font-bold leading-none sm:text-5xl">You can test your  
+                        <span className="dark:text-violet-400"> React JavaScript CSS Git</span>skills with DEVELOPER-GRID' Quiz.
                     </h1>
-                    <p className="px-8 mt-8 mb-12 text-lg">Cupiditate minima voluptate temporibus quia? Architecto beatae esse ab amet vero eaque explicabo!</p>
+                    <p className="px-8 mt-8 mb-12 text-lg">You will get 1 point for each correct answer. At the end of the Quiz, your total score will be displayed. The maximum score is 40 points.</p>
                     <div className="flex flex-wrap justify-center">
-                        <button className="px-8 py-3 m-2 text-lg border rounded dark:text-gray-50 dark:border-gray-700">Learn more</button>
+                       <Link to="/blog"> <button className="px-8 py-3 m-2 text-lg border rounded dark:text-gray-50 dark:border-gray-700">Learn more</button></Link>
                     </div>
                 </div>
             </section>
