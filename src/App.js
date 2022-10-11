0194a0befc811284ebf6433589bc51c8e46fa3b7
Bikrom-Roy <bikromroy2001@gmail.com>
2022-10-11 11:36:26 +0600

    Statistics page create

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Blog from './components/Blog/Blog';
 import Error from './components/Error/Error';
 import Home from './components/Home/Home';
+import Statistics from './components/Statistics/Statistics';
 import Topics from './components/Topics/Topics';
 import Main from './layout/Main';
 
@@ -28,6 +29,11 @@ function App() {
         {
           path: '/topics',
           element: <Topics></Topics>,
+        },
+        {
+          path: '/statistics',
+          loader: async()=> fetch('https://openapi.programming-hero.com/api/quiz'),
+          element: <Statistics></Statistics>
         },
         {
           path: '/blog',
