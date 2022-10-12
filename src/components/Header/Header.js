f717757822d16c9ed28903eb7fb7ad2b458ef582
Bikrom-Roy <bikromroy2001@gmail.com>
2022-10-12 12:12:57 +0600

    end website

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -132,7 +132,7 @@ const Header = () => {
                     <ul className='space-y-4'>
                       <li>
                         <Link
-                          to='/topics'
+                          to='/'
                           aria-label='Topics'
                           title='Topics'
                           className='font-medium tracking-wide text-gray-700 transition-colors duration-200 hover:text-deep-purple-accent-400'
